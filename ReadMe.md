66508cea490081b93f08a4d5cbc9ef07aae2cfa4
Oriol Ferrer Mesià <oriol@uri.cat>
2013-08-23 13:53:50 -0400

    added missing files

---
+++ ReadMe.md
@@ -0,0 +1,49 @@
+#ofxFboBlur
+
+blur things easily! Render your scene into the ofxFboBlur object; it will keep your original scene, and a blurred copy of it. This way, you can draw your "clean" scene and overlay any amount of blur you want on top of it.
+
+<img src="http://f.cl.ly/items/0A133I2e030w0L0B1c3M/Screen%20Shot%202013-08-23%20at%2013.39.58.PNG"/>
+
+```
+	//choose your fbo settings
+	ofFbo::Settings s;
+	s.width = ofGetWidth();
+	s.height = ofGetHeight();
+	s.internalformat = GL_RGBA;
+	s.maxFilter = GL_LINEAR; GL_NEAREST;
+	s.numSamples = 4;
+	s.numColorbuffers = 3;
+	s.useDepth = true;
+	s.useStencil = false;
+
+	//setup thing
+	gpuBlur.setup(s);
+
+	//tweak your blurring	
+	gpuBlur.blurOffset = 5 * ofMap(mouseY, 0, ofGetHeight(), 1, 0, true);
+	gpuBlur.blurPasses = 10. * ofMap(mouseX, 0, ofGetWidth(), 0, 1, true);
+	gpuBlur.numBlurOverlays = 1;
+	gpuBlur.blurOverlayGain = 255;
+
+	//draw your scene in the fbo, no drawing on screen yet
+	gpuBlur.beginDrawScene();
+		//YOUR STUFF HERE!
+	gpuBlur.endDrawScene();
+
+	//calc the fbo blurring, no drawing on screen yet
+	gpuBlur.performBlur();
+
+	//draw the "clean" scene on screen
+	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+	gpuBlur.drawSceneFBO();
+
+	//overlay the blurred fbo on top of the previously drawn clean scene
+	ofEnableBlendMode(OF_BLENDMODE_ADD);
+	gpuBlur.drawBlurFbo();
+
+	
+	
+
+
+```
+
---
+++ example/testApp.cpp
@@ -0,0 +1,107 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+	ofBackground(22);
+	ofFbo::Settings s;
+	s.width = ofGetWidth();
+	s.height = ofGetHeight();
+	s.internalformat = GL_RGBA;
+	//	s.textureTarget = GL_TEXTURE_RECTANGLE_ARB;
+	s.maxFilter = GL_LINEAR; GL_NEAREST;
+	s.numSamples = 4;
+	s.numColorbuffers = 3;
+	s.useDepth = true;
+	s.useStencil = false;
+
+	gpuBlur.setup(s);
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+	gpuBlur.blurOffset = 5 * ofMap(mouseY, 0, ofGetHeight(), 1, 0, true);
+	gpuBlur.blurPasses = 10. * ofMap(mouseX, 0, ofGetWidth(), 0, 1, true);
+	gpuBlur.numBlurOverlays = 1;
+	gpuBlur.blurOverlayGain = 255;
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	ofSetColor(255);
+	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+	gpuBlur.beginDrawScene();
+		ofClear(0, 0, 0, 0);
+		ofCircle(ofGetWidth()/2, ofGetHeight()/2,  100);
+	gpuBlur.endDrawScene();
+
+	//blur the fbo
+	gpuBlur.performBlur();
+
+	//draw the "clean" scene
+	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+	gpuBlur.drawSceneFBO();
+
+	//overlay the blur on top
+	ofEnableBlendMode(OF_BLENDMODE_ADD);
+	gpuBlur.drawBlurFbo();
+
+	//draw info
+	string info =	"blurOffset: " + ofToString(gpuBlur.blurOffset) + "\n" +
+					"blurPasses: " + ofToString(gpuBlur.blurPasses) + "\n" +
+					"numBlurOverlays: " + ofToString(gpuBlur.numBlurOverlays) + "\n" +
+	"blurOverlayGain: " + ofToString(gpuBlur.blurOverlayGain);
+	ofDrawBitmapStringHighlight(info, 20,20);
+
+	ofDrawBitmapStringHighlight("MouseX to control Blur Passes\nMouseY to control blur Offset", 400,20);
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
